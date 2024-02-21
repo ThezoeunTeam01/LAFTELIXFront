d095d38e74567956bb30053763aad49e40a99736
STARella01 <seongee2rum@gmail.com>
2024-02-21 16:31:28 +0900

    콘피그 설정 수정 - 백엔드 로컬이랑 실서버 구분 추가

--- src/api-config.tsx
+++ src/api-config.tsx
@@ -4,6 +4,10 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   backendHost = "http://localhost:8081";
-}
+}else{
+  // 로컬 호스트가 아닌 경우 - 실제 서버로..
+  //backendHost = "http://todo-aws-backend.ap-northeast-2.elasticbea.com"; // 백엔트 호스트 aws 백엔드와 연결
+  backendHost = "https://api.laftelixx.p-e.kr"; // 백엔트 호스트 aws 백엔드와 연결
+} 
 
 export const API_BASE_URL = `${backendHost}`;
