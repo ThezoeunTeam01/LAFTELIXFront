54304f74a7b37f46e63e870fab94aa1b38147b8b
STARella01 <seongee2rum@gmail.com>
2024-02-22 18:47:35 +0900

    메인 슬라이드 모달 오픈 영역 수정

--- src/main/MainSlide.tsx
+++ src/main/MainSlide.tsx
@@ -78,7 +78,10 @@ const MainSlide: FC = () => {
     // interval={null} - 슬라이드 멈추고 싶으면 추가
     <Carousel fade className="" interval={null}>
       {contents.map((content: Content) => (
-        <CarouselItem key={content.id} style={{ cursor: `pointer` }}>
+        <CarouselItem key={content.id} style={{ cursor: `pointer` }} 
+        onClick={() => {
+          openModal(content.id, content.media_type);
+        }}>
           <div className="vw-100 vh-100">
             <div className="w-100 h-100 position-absolute top-0 start-0 dimBg"></div>
             <img
@@ -90,11 +93,7 @@ const MainSlide: FC = () => {
           </div>
 
           <Carousel.Caption>
-            <Container
-              onClick={() => {
-                openModal(content.id, content.media_type);
-              }}
-            >
+            <Container>
               <div
                 className="d-flex flex-column justify-content-end"
                 style={{ width: `45%`, marginBottom: `100px` }}
